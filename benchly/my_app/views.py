8687cf26a32f2bf83b67b2745663ff20fd9f5ce7
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2022-11-16 21:31:23 -0800

    Added ability to make a plot with plotly

--- benchly/my_app/views.py
+++ benchly/my_app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
 #from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    x_data = [0,1,2,3]
+    y_data = [x**2 for x in x_data]
+    plot_div = plot([Scatter(x=x_data, y=y_data,
+                        mode='lines', name='test',
+                        opacity=0.8, marker_color='green')],
+               output_type='div')
+    return render(request, "home.html", context={'plot_div': plot_div})
+    # return render(request, 'home.html')
 
 #def index(request):
 #    return HttpResponse('Hello, World!')
