3bc50f763d524df130b7e22a52f8a3be8f72790d
Penny Rowe <1795738+prowe12@users.noreply.github.com>
2022-12-15 13:56:09 -0800

    Continue removing __pycache__ files

--- benchlysite/scripts/load_outputs.py
+++ benchlysite/scripts/load_outputs.py
@@ -16,7 +16,18 @@ def run():
             clim_outputs = ClimOutputs(scenario=ClimInputs.objects.get(scenario=int(row[0])),
                         year=row[1],
                         atmos_co2=row[2],
-                        ocean_co2=row[3])
+                        ocean_co2=row[3],
+                        ocean_ph=row[4],
+                        t_C = row[5],
+                        t_F = row[6],
+                        t_anomaly = row[7],
+                        f_ha = row[8],
+                        f_ao = row[9],
+                        f_oa = row[10],
+                        f_al = row[11],
+                        f_la = row[12],
+                        tot_ha = row[13]
+)
             clim_outputs.save()
 
             # new_team = Team(
